560453abdb2d386726195e8a98746db8b96108a5
Antonio Brych <63557494+antoniobrych@users.noreply.github.com>
2025-03-20 12:55:32 -0300

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Mike Wazowski</h1>
+<img src="./images/mike.png" alt="Mike Wazowski" width="500px">
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>Contact me</title>
+</svelte:head>
+<form action="mailto:abrych@arborcapital.com.br" method="GET">
+<label for="email">Email: <input id="email" type="email" name="text_email"> </label>
+<label for="subject">Subject: <input id="subject" type="text" name="text_subject"> </label>
+<label for="body">Body: <textarea id="body" name="text_message"></textarea> </label>
+
+<input type="submit" value="Submit" id="button">
+</form>
